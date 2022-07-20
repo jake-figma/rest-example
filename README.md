2aee867fdfb49dfeb499942eca0289002ea2636e
Jake Albaugh <jalbaugh@figma.com>
2022-07-20 15:24:28 -0500

    hello world

---
+++ README.md
@@ -0,0 +1,3 @@
+# REST API in a plugin Example
+
+Getting all components from a file, showing them in a ui, allowing the user to click or drag them into the file.
---
+++ code.js
@@ -0,0 +1,31 @@
+const TOKEN = "figd_YOURPERSONAL-ACCESSTOKEN";
+const FILE_ID = "FILE_ID_FROM_URL";
+
+figma.ui.onmessage = (message) => loadComponent(message);
+
+figma.on("drop", ({ x, y, items }) => {
+  if (items[0]) {
+    console.log(x, y);
+    loadComponent(items[0], x, y);
+  }
+});
+
+async function loadComponent({ type, data }, x, y) {
+  try {
+    if (type === "error") {
+      throw { message: data };
+    } else if (type === "component") {
+      const node = await figma.importComponentByKeyAsync(data);
+      const cloned = node.clone();
+      console.log(x, y);
+      cloned.x = x !== undefined ? x : figma.viewport.center.x;
+      cloned.y = y !== undefined ? y : figma.viewport.center.y;
+    }
+  } catch (e) {
+    figma.notify(e.message, { error: true });
+    figma.closePlugin();
+  }
+}
+
+figma.showUI(__html__, { height: 300, visible: true });
+figma.ui.postMessage({ fileId: FILE_ID, token: TOKEN });
