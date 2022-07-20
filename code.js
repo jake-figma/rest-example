017a263c0bfa90db3578eef5cb5d337ff9cf65f0
Jake Albaugh <jalbaugh@figma.com>
2022-07-20 17:18:29 -0500

    updates

--- code.js
+++ code.js
@@ -5,7 +5,6 @@ figma.ui.onmessage = (message) => loadComponent(message);
 
 figma.on("drop", ({ x, y, items }) => {
   if (items[0]) {
-    console.log(x, y);
     loadComponent(items[0], x, y);
   }
 });
@@ -15,13 +14,23 @@ async function loadComponent({ type, data }, x, y) {
     if (type === "error") {
       throw { message: data };
     } else if (type === "component") {
-      const node = await figma.importComponentByKeyAsync(data);
-      const cloned = node.createInstance();
-      cloned.x = x !== undefined ? x : figma.viewport.center.x;
-      cloned.y = y !== undefined ? y : figma.viewport.center.y;
+      const component = await figma.importComponentByKeyAsync(data);
+      const instance = component.createInstance();
+      instance.x = x !== undefined ? x : figma.viewport.center.x;
+      instance.y = y !== undefined ? y : figma.viewport.center.y;
+      console.log(instance.componentProperties);
+      console.log(component.parent.componentPropertyDefinitions);
+      // how to get default properties
+      // const defaultProperties = {};
+      // for (let key in component.parent.componentPropertyDefinitions) {
+      //   defaultProperties[key] =
+      //     component.parent.componentPropertyDefinitions[key].defaultValue;
+      // }
+      // instance.setProperties(defaultProperties);
     }
   } catch (e) {
-    figma.notify(e.message, { error: true });
+    figma.notify("Error", { error: true });
+    console.error(e.message, { error: true });
     figma.closePlugin();
   }
 }
